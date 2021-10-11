57a5f5532dc3d11a90facf12dd2e28b2ed1a2d6b
krobingit <91210322+krobingit@users.noreply.github.com>
2021-10-12 04:25:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+console.log("started timer")
+setTimeout(() => {
+    document.querySelector(".countdown").innerText = "10";
+    setTimeout(() => {
+        document.querySelector(".countdown").innerText = "9";
+        setTimeout(() => {
+            document.querySelector(".countdown").innerText = "8";
+            setTimeout(() => {
+                document.querySelector(".countdown").innerText = "7";
+                setTimeout(() => {
+                    document.querySelector(".countdown").innerText = "6";
+
+                    setTimeout(() => {
+                        document.querySelector(".countdown").innerText = "5";
+                        setTimeout(() => {
+                            document.querySelector(".countdown").innerText = "4";
+                            setTimeout(() => {
+                                document.querySelector(".countdown").innerText = "3";
+                                setTimeout(() => {
+                                    document.querySelector(".countdown").innerText = "2";
+                                    setTimeout(() => {
+                                        document.querySelector(".countdown").innerText = "1";
+                                        setTimeout(() => {
+                                            document.querySelector(".countdown").innerText = "Happy Independence Day";
+
+
+
+
+                                        }, 1000);
+
+
+
+
+
+                                    }, 1000);
+
+
+
+
+                                }, 1000);
+
+
+
+
+                            }, 1000);
+
+
+
+
+                        }, 1000);
+
+
+
+
+
+                    }, 1000);
+
+
+
+
+
+
+                }, 1000);
+
+
+
+
+            }, 1000);
+
+
+
+
+        }, 1000);
+
+
+    }, 1000);
+
+
+
+}, 1000);
+
+
+
+
